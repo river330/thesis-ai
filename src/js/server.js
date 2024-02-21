52bd61ee6665918849b33414e76572914a7e5ad1
randrivera <rand.k.rivera@gmail.com>
2024-02-20 20:58:02 -0500

    updated to module

--- src/js/server.js
+++ src/js/server.js
@@ -1,4 +1,5 @@
-const http = require('http');
+import * as http from 'http';
+// const http = require('http');
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type": "text/plain"});
